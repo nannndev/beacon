e86f2d70797dc7cc288fda44331a8c59d54a0f86
naneps <ekaprasetya2244@gmail.com>
2026-07-28 09:52:59 +0700

    Add Vercel Analytics to landing

--- landing/src/main.tsx
+++ landing/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import LandingPage from './pages/LandingPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LandingPage />
+    <Analytics />
   </React.StrictMode>,
 )
